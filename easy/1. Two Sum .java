85298c2dab24fe36b7b44f555106655d01281c11
yuxin8 <zhangyuxin4123671@gmail.com>
2021-04-30 11:37:00 -0500

    Create 1. Two Sum .java

---
+++ easy/1. Two Sum .java
@@ -0,0 +1,23 @@
+/*
+Complexity Analysis:
+
+Time complexity : O(n). We traverse the list containing nn elements only once. Each look up in the table costs only O(1) time.
+
+Space complexity : O(n). The extra space required depends on the number of items stored in the hash table, which stores at most n elements.
+*/
+
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+
+            int complement = target - nums[i];
+            if ( map.containsKey(complement) ) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);      
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
